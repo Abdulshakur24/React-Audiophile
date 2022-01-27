3b5d99177bd6dfbcc7e557e4327da5ae49e2afc7
abdulshakur <abdulshakur486@gmail.com>
2022-01-27 22:15:40 +0300

    All components are exported from here.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export { default as Banner } from "./Banner";
+export { default as Products } from "./Products";
+export { default as Galleries } from "./Galleries";
+export { default as Description } from "./Description";
+export { default as Footer } from "./Footer";
